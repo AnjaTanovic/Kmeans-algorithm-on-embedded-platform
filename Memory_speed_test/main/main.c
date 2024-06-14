d0627e547b5a703438ac37a23706fee56abf0bda
AnjaTanovic <anjatanovic99@gmail.com>
2024-06-14 11:31:11 +0200

    Testing memory types on 4 files and for 250*4 arrays for dataset in PSRAM

--- Memory_speed_test/main/main.c
+++ Memory_speed_test/main/main.c
@@ -17,6 +17,8 @@
 #include "esp_vfs_fat.h"
 #include "driver/sdmmc_host.h"
 
+#include <string.h>
+
 /*****************************************************************************************************************************************/
 //CPU frequency = 240 MHz (Component config -> ESP System Settings)
 //Flash SPI speed = 80 MHz, Flash size = 4 MB (Serial flasher config)
@@ -41,17 +43,49 @@ static const char *TAG_sd = "SD";
 
 #define DIM 196
 #define NUM_OF_POINTS_PER_FILE 250
+#define NUM_OF_FILES 4
 
+//buffers in internal RAM
 uint8_t dataCoor[NUM_OF_POINTS_PER_FILE][DIM];				
 uint8_t dataCluster[NUM_OF_POINTS_PER_FILE];			
 uint8_t dataLabel[NUM_OF_POINTS_PER_FILE];
 
 #ifdef TEST_CASE_PSRAM
-EXT_RAM_BSS_ATTR uint8_t readData[NUM_OF_POINTS_PER_FILE*(DIM+2)];	//array in external RAM (PSRAM) where dataset is stored			
+//arrays in external RAM (PSRAM) where dataset is stored
+EXT_RAM_BSS_ATTR uint8_t psramCoor[NUM_OF_FILES * NUM_OF_POINTS_PER_FILE][DIM];			
+EXT_RAM_BSS_ATTR uint8_t psramCluster[NUM_OF_FILES * NUM_OF_POINTS_PER_FILE];
+EXT_RAM_BSS_ATTR uint8_t psramLabel[NUM_OF_FILES * NUM_OF_POINTS_PER_FILE];					
 #endif
 
-char readPath[30] = "/read.bin";     //img_0.bin from mnist dataset (250 images)
-char writePath[30] = "/write.bin";   //empty file
+char readPath[30] = "/read";     //img_0.bin from mnist dataset (250 images)
+char writePath[30] = "/write";   //empty file
+
+void esp32printInfo() {
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    uint32_t flash_size;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return;
+    }
+
+    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+}
 
 void testFLASH() {
     //Variables for time measuring
@@ -111,61 +145,75 @@ void testFLASH() {
     printf("* Reading:\n");
     
     char new_read_path_flash[50]; 
-	sprintf(new_read_path_flash, "/spiffs%s", readPath);
     FILE* f;
 
     //Start measuring time
     start_time = esp_timer_get_time();
-        
-    f = fopen(new_read_path_flash, "rb");
-    if (f == NULL) {
-        ESP_LOGE(TAG_flash, "Failed to open file for reading");
-        return;
+
+    //Read data from all files
+    for (uint8_t num_of_file = 0; num_of_file < NUM_OF_FILES; num_of_file++) {
+        sprintf(new_read_path_flash, "/spiffs%s_%d.bin", readPath, num_of_file);
+        f = fopen(new_read_path_flash, "rb");
+        if (f == NULL) {
+            ESP_LOGE(TAG_flash, "Failed to open file for reading");
+            return;
+        }
+        for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
+            //read label
+            fread(&dataLabel[currentPoint], 1, 1, f);
+
+            //read coordinates
+            fread(dataCoor[currentPoint], 1, DIM, f);
+
+            //read cluster
+            fread(&dataCluster[currentPoint], 1, 1, f);
+        }
+        fclose(f);
     }
-    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
-        //read label
-        fread(&dataLabel[currentPoint], 1, 1, f);
-
-        //read coordinates
-        fread(dataCoor[currentPoint], 1, DIM, f);
-
-        //read cluster
-        fread(&dataCluster[currentPoint], 1, 1, f);
-	}
-    fclose(f); 
 
     //Finish measuring time
     end_time = esp_timer_get_time();
-    printf("Time spent for reading: %llu microseconds\n\n", end_time - start_time); 
+    uint64_t duration = end_time - start_time;
+    printf("Time spent for reading: %llu microseconds\n", duration); 
+    uint16_t bytes = NUM_OF_POINTS_PER_FILE * (DIM + 2);
+    double speed = (bytes / (double)duration) * 1000000.0 / (1024.0 * 1024.0); // Speed in megabytes per second
+    printf("Read speed: %.6lf MB/s\n\n", speed); 
          
     printf("* Writing:\n");
 
     char new_write_path_flash[50]; 
-	sprintf(new_write_path_flash, "/spiffs%s", writePath);
 
     //Start measuring time
-    start_time = esp_timer_get_time();        
+    start_time = esp_timer_get_time();     
 
-    f = fopen(new_write_path_flash, "wb");
-    if (f == NULL) {
-        ESP_LOGE(TAG_flash, "Failed to open file for writing");
-        return;
+    //Write data to all files
+    for (uint8_t num_of_file = 0; num_of_file < NUM_OF_FILES; num_of_file++) {
+	    sprintf(new_write_path_flash, "/spiffs%s_%d.bin", writePath, num_of_file);
+        f = fopen(new_write_path_flash, "wb");
+        if (f == NULL) {
+            ESP_LOGE(TAG_flash, "Failed to open file for writing");
+            return;
+        }
+        for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
+            //write label
+            fwrite(&dataLabel[currentPoint], 1, 1, f);
+
+            //write coordinates
+            fwrite(dataCoor[currentPoint], 1, DIM, f);
+
+            //write cluster
+            fwrite(&dataCluster[currentPoint], 1, 1, f);
+        }
+        fclose(f);
     }
-    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
-        //write label
-        fwrite(&dataLabel[currentPoint], 1, 1, f);
-
-        //write coordinates
-        fwrite(dataCoor[currentPoint], 1, DIM, f);
-
-        //write cluster
-        fwrite(&dataCluster[currentPoint], 1, 1, f);
-	}
-    fclose(f);
 
     //Finish measuring time
     end_time = esp_timer_get_time();
-    printf("Time spent for writing: %llu microseconds\n\n", end_time - start_time);    
+    duration = end_time - start_time;
+    printf("Time spent for writing: %llu microseconds\n", duration); 
+    bytes = NUM_OF_POINTS_PER_FILE * (DIM + 2);
+    speed = (bytes / (double)duration) * 1000000.0 / (1024.0 * 1024.0); // Speed in megabytes per second
+    printf("Write speed: %.6lf MB/s\n\n", speed);  
 
     // All done, unmount partition and disable SPIFFS
     esp_vfs_spiffs_unregister(conf.partition_label);
@@ -178,51 +226,79 @@ void testPSRAM() {
 
     printf("\n******** Test case PSRAM ********\n");
 
-    //Fill array in PSRAM with data from image
-    for (uint16_t i = 0; i < NUM_OF_POINTS_PER_FILE*(DIM+2); i++)
-        readData[i] = i;
+    //Start measuring time
+    start_time = esp_timer_get_time();	
+
+    //Fill array in PSRAM with random data
+    for (uint16_t i = 0; i < NUM_OF_FILES * NUM_OF_POINTS_PER_FILE; i++) {
+        psramCluster[i] = i;
+        psramLabel[i] = (NUM_OF_POINTS_PER_FILE * NUM_OF_FILES) - i;
+        for (uint8_t j = 0; j < DIM; j++)
+            psramCoor[i][j] = j;
+    }
+
+    //Finish measuring time
+    end_time = esp_timer_get_time();
+    uint64_t duration = end_time - start_time;
+    printf("Time spent for initial writing: %llu microseconds\n", duration); 
+    uint16_t bytes = NUM_OF_POINTS_PER_FILE * (DIM + 2);
+    double speed = (bytes / (double)duration) * 1000000.0 / (1024.0 * 1024.0); // Speed in megabytes per second
+    printf("Write speed: %.6lf MB/s\n\n", speed);     
 
     printf("* Reading:\n");
 
    	//Start measuring time
     start_time = esp_timer_get_time();
         
-    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
+    //Read data from dataset
+    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_FILES * NUM_OF_POINTS_PER_FILE; currentPoint++) {
         //read label
-        dataLabel[currentPoint] = readData[currentPoint*(DIM+2)];
+        dataLabel[currentPoint % NUM_OF_POINTS_PER_FILE] = psramLabel[currentPoint];
 
         //read coordinates
+        /*
         for (uint16_t coor = 0; coor < DIM; coor++)
-            dataCoor[currentPoint][coor] = readData[currentPoint*(DIM+2) + coor + 1];
+           dataCoor[currentPoint][coor] = psramCoor[currentPoint][coor];
+        */
+        memcpy(dataCoor[currentPoint % NUM_OF_POINTS_PER_FILE], psramCoor[currentPoint], DIM);
 
         //read cluster
-        dataCluster[currentPoint] = readData[currentPoint*(DIM+2) + DIM + 1];
+        dataCluster[currentPoint % NUM_OF_POINTS_PER_FILE] = psramCluster[currentPoint];;
 	}
 
     //Finish measuring time
     end_time = esp_timer_get_time();
-    printf("Time spent for reading: %llu microseconds\n\n", end_time - start_time); 
+    duration = end_time - start_time;
+    printf("Time spent for reading: %llu microseconds\n", duration); 
+    bytes = NUM_OF_POINTS_PER_FILE * (DIM + 2);
+    speed = (bytes / (double)duration) * 1000000.0 / (1024.0 * 1024.0); // Speed in megabytes per second
+    printf("Read speed: %.6lf MB/s\n\n", speed);  
          
     printf("* Writing:\n");
 
     //Start measuring time
     start_time = esp_timer_get_time();	
         
-    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
+    //Write to all points
+    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_FILES * NUM_OF_POINTS_PER_FILE; currentPoint++) {
         //write label
-        readData[currentPoint*(DIM+2)] = currentPoint;
+        psramLabel[currentPoint] = currentPoint;
 
         //write coordinates
         for (uint16_t coor = 0; coor < DIM; coor++)
-            readData[currentPoint*(DIM+2) + coor + 1] = currentPoint;
+            psramCoor[currentPoint][coor] = coor;
 
         //write cluster
-        readData[currentPoint*(DIM+2) + DIM + 1] = currentPoint;
+        psramCluster[currentPoint] = currentPoint;
 	}
 
     //Finish measuring time
     end_time = esp_timer_get_time();
-    printf("Time spent for writing:: %llu microseconds\n\n", end_time - start_time); 
+    duration = end_time - start_time;
+    printf("Time spent for writing: %llu microseconds\n", duration); 
+    bytes = NUM_OF_POINTS_PER_FILE * (DIM + 2);
+    speed = (bytes / (double)duration) * 1000000.0 / (1024.0 * 1024.0); // Speed in megabytes per second
+    printf("Write speed: %.6lf MB/s\n\n", speed);  
 }
 
 void testSD() {
@@ -262,61 +338,75 @@ void testSD() {
     printf("* Reading:\n");
 
     char new_read_path_sd[50]; 
-	sprintf(new_read_path_sd, "/sdcard%s", readPath);
     FILE* f;
 
     //Start measuring time
     start_time = esp_timer_get_time();
-        
-    f = fopen(new_read_path_sd, "rb");
-    if (f == NULL) {
-        ESP_LOGE(TAG_flash, "Failed to open file for reading");
-        return;
-    }   
-    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
-        //read label
-        fread(&dataLabel[currentPoint], 1, 1, f);
 
-        //read coordinates
-        fread(dataCoor[currentPoint], 1, DIM, f);
+    //Read data from all files
+    for (uint8_t num_of_file = 0; num_of_file < NUM_OF_FILES; num_of_file++) {
+	    sprintf(new_read_path_sd, "/sdcard%s_%d.bin", readPath, num_of_file);
+        f = fopen(new_read_path_sd, "rb");
+        if (f == NULL) {
+            ESP_LOGE(TAG_flash, "Failed to open file for reading");
+            return;
+        }   
+        for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
+            //read label
+            fread(&dataLabel[currentPoint], 1, 1, f);
 
-        //read cluster
-        fread(&dataCluster[currentPoint], 1, 1, f);
-	}
-    fclose(f); 
+            //read coordinates
+            fread(dataCoor[currentPoint], 1, DIM, f);
+
+            //read cluster
+            fread(&dataCluster[currentPoint], 1, 1, f);
+        }
+        fclose(f);
+    }
 
     //Finish measuring time
     end_time = esp_timer_get_time();
-    printf("Time spent for reading: %llu microseconds\n\n", end_time - start_time); 
+    uint64_t duration = end_time - start_time;
+    printf("Time spent for reading: %llu microseconds\n", duration); 
+    uint16_t bytes = NUM_OF_POINTS_PER_FILE * (DIM + 2);
+    double speed = (bytes / (double)duration) * 1000000.0 / (1024.0 * 1024.0); // Speed in megabytes per second
+    printf("Read speed: %.6lf MB/s\n\n", speed); 
          
     printf("* Writing:\n");
 
     char new_write_path_sd[50]; 
-	sprintf(new_write_path_sd, "/sdcard%s", writePath);
 
     //Start measuring time
-    start_time = esp_timer_get_time();        
+    start_time = esp_timer_get_time();     
 
-    f = fopen(new_write_path_sd, "wb");
-    if (f == NULL) {
-        ESP_LOGE(TAG_sd, "Failed to open file for writing");
-        return;
+    //Write data to all files
+    for (uint8_t num_of_file = 0; num_of_file < NUM_OF_FILES; num_of_file++) {
+	    sprintf(new_write_path_sd, "/sdcard%s_%d.bin", writePath, num_of_file);
+        f = fopen(new_write_path_sd, "wb");
+        if (f == NULL) {
+            ESP_LOGE(TAG_sd, "Failed to open file for writing");
+            return;
+        }
+        for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
+            //write label
+            fwrite(&dataLabel[currentPoint], 1, 1, f);
+
+            //write coordinates
+            fwrite(dataCoor[currentPoint], 1, DIM, f);
+
+            //write cluster
+            fwrite(&dataCluster[currentPoint], 1, 1, f);
+        }
+        fclose(f);
     }
-    for (uint16_t currentPoint = 0; currentPoint < NUM_OF_POINTS_PER_FILE; currentPoint++) {
-        //write label
-        fwrite(&dataLabel[currentPoint], 1, 1, f);
-
-        //write coordinates
-        fwrite(dataCoor[currentPoint], 1, DIM, f);
-
-        //write cluster
-        fwrite(&dataCluster[currentPoint], 1, 1, f);
-	}
-    fclose(f);
 
     //Finish measuring time
     end_time = esp_timer_get_time();
-    printf("Time spent for writing: %llu microseconds\n\n", end_time - start_time);        
+    duration = end_time - start_time;
+    printf("Time spent for writing: %llu microseconds\n", duration); 
+    bytes = NUM_OF_POINTS_PER_FILE * (DIM + 2);
+    speed = (bytes / (double)duration) * 1000000.0 / (1024.0 * 1024.0); // Speed in megabytes per second
+    printf("Write speed: %.6lf MB/s\n\n", speed);       
 
     // All done, unmount sd card
     esp_vfs_fat_sdcard_unmount(mount_point, card);
@@ -329,32 +419,7 @@ void app_main(void)
 
     printf("Running program on esp32...\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    /*********************************** TEST CASES ***********************************/
+    esp32printInfo();
 
     #ifdef TEST_CASE_FLASH
         testFLASH();
